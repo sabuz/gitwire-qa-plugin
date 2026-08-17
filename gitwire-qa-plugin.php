56004484875b81312e3007c0a27f57dbab5841c2
Gitwire QA <qa@example.com>
2026-08-17 23:58:57 +0600

    v1.0.6: restore to a healthy state after QA

--- gitwire-qa-plugin.php
+++ gitwire-qa-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Gitwire QA Test Plugin
  * Description: Scratch plugin used to QA gitwire's install/update/fatal-guard flows. Safe to delete.
- * Version: 1.0.5
+ * Version: 1.0.6
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -10,9 +10,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 add_action( 'admin_notices', function () {
-	echo '<div class="notice notice-info"><p>Gitwire QA test plugin v1.0.5.</p></div>';
+	echo '<div class="notice notice-info"><p>Gitwire QA test plugin v1.0.6 (healthy).</p></div>';
 } );
-
-function gitwire_qa_fix_verification_break() {
-	echo "unterminated
-}
