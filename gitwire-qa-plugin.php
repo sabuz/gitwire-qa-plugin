71a919e09e0835dcd76b1085a1a543c08ffd11c3
Gitwire QA <qa@example.com>
2026-08-17 23:04:11 +0600

    v1.0.4: trivial bump to trigger an update for backup QA

--- gitwire-qa-plugin.php
+++ gitwire-qa-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Gitwire QA Test Plugin
  * Description: Scratch plugin used to QA gitwire's install/update/fatal-guard flows. Safe to delete.
- * Version: 1.0.3
+ * Version: 1.0.4
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -10,5 +10,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 add_action( 'admin_notices', function () {
-	echo '<div class="notice notice-info"><p>Gitwire QA test plugin v1.0.3 active (fixed).</p></div>';
+	echo '<div class="notice notice-info"><p>Gitwire QA test plugin v1.0.4 active (fixed).</p></div>';
 } );
